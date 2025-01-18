530610f89c4ebacfd48d5cd19d8fd43a26f162bd
DerrickXu1998 <dyuxu01@gmail.com>
2025-01-18 15:21:18 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import LandingPageLayout from "@/components/Layouts/LandingPageLayout";
 
 export const metadata: Metadata = {
   title:
@@ -10,10 +10,14 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
-      <DefaultLayout>
-        <ECommerce />
-      </DefaultLayout>
-    </>
+    <LandingPageLayout>
+      <main className="flex flex-1 flex-col items-center justify-center p-6 text-center md:flex-row md:space-x-8 md:text-left">
+        <div className="flex max-w-md flex-col gap-8">
+          <h1 className="font-title text-4xl md:text-5xl">Measure 10<strong>⤫</strong> the carbon emission in 1/10<sup>th</sup> the time</h1>
+          <p className="font-title text-xl text-muted-foreground">Node.0 delivers real-time insights with our ESG Carbon Emission Platform. Track, reduce, and optimize your carbon footprint to stay ahead of regulations and drive sustainable impact.</p>
+        </div>
+        <img className="mt-8 hidden w-full max-w-lg md:mt-0 md:block" src={"/images/image1.svg"}></img>
+      </main>
+    </LandingPageLayout>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,26 @@ import DropdownNotification from "./DropdownNotification";
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
 
+const requireAuth = () => {
+  if (!localStorage.getItem('token')) {
+    // go to login route
+    return false
+  }
+  return true
+  // stay on this route since the user is authenticated
+}
+
+const signInButton = () => {
+  // TODO: add a path for sign in
+  return (<Link
+    href="#"
+    className="hover:bg-primary hover:bg-opacity-10 inline-flex items-center justify-center rounded-md border border-primary px-6 py-2 text-center font-medium text-primary hover:bg-opacity-90 lg:px-8 xl:px-10"
+  >
+    Sign In
+  </Link>)
+
+}
+
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
   setSidebarOpen: (arg0: boolean) => void;
@@ -12,9 +32,10 @@ const Header = (props: {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
-        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
+        <div className="flex items-center gap-2 sm:gap-4 ">
+          <h1 className="text-xl font-bold color:black">Node.0</h1>
           {/* <!-- Hamburger Toggle BTN --> */}
-          <button
+          {/* <button
             aria-controls="sidebar"
             onClick={(e) => {
               e.stopPropagation();
@@ -25,50 +46,36 @@ const Header = (props: {
             <span className="relative block h-5.5 w-5.5 cursor-pointer">
               <span className="du-block absolute right-0 h-full w-full">
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!w-full delay-300"
-                  }`}
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${!props.sidebarOpen && "!w-full delay-300"
+                    }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "delay-400 !w-full"
-                  }`}
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${!props.sidebarOpen && "delay-400 !w-full"
+                    }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!w-full delay-500"
-                  }`}
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${!props.sidebarOpen && "!w-full delay-500"
+                    }`}
                 ></span>
               </span>
               <span className="absolute right-0 h-full w-full rotate-45">
                 <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!h-0 !delay-[0]"
-                  }`}
+                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${!props.sidebarOpen && "!h-0 !delay-[0]"
+                    }`}
                 ></span>
                 <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!h-0 !delay-200"
-                  }`}
+                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${!props.sidebarOpen && "!h-0 !delay-200"
+                    }`}
                 ></span>
               </span>
             </span>
-          </button>
+          </button> */}
           {/* <!-- Hamburger Toggle BTN --> */}
-
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
-            <Image
-              width={32}
-              height={32}
-              src={"/images/logo/logo-icon.svg"}
-              alt="Logo"
-            />
-          </Link>
         </div>
 
-        <div className="hidden sm:block">
+        {/* <div className="hidden sm:block">
           <form action="https://formbold.com/s/unique_form_id" method="POST">
-            {/* <div className="relative">
+            <div className="relative">
               <button className="absolute left-0 top-1/2 -translate-y-1/2">
                 <svg
                   className="fill-body hover:fill-primary dark:fill-bodydark dark:hover:fill-primary"
@@ -98,9 +105,9 @@ const Header = (props: {
                 placeholder="Type to search..."
                 className="w-full bg-transparent pl-9 pr-4 font-medium focus:outline-none xl:w-125"
               />
-            </div> */}
+            </div>
           </form>
-        </div>
+        </div> */}
 
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
@@ -118,7 +125,7 @@ const Header = (props: {
           </ul>
 
           {/* <!-- User Area --> */}
-          <DropdownUser />
+          {requireAuth() ? <DropdownUser /> : signInButton()}
           {/* <!-- User Area --> */}
         </div>
       </div>
